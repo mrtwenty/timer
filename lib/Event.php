fd9899d9b0629cb6ceef0d0ee884af8a407b5f7c
dazhaozhao <446390206@qq.com>
2019-08-16 14:26:45 +0800

    调整

--- lib/Event.php
+++ lib/Event.php
@@ -136,33 +136,3 @@ class Event implements LibInterface
         return count($this->_eventTimer);
     }
 }
-
-//测试执行 timer类
-
-function microtime_float()
-{
-    list($usec, $sec) = explode(" ", microtime());
-    return bcadd($usec, $sec, 3);
-}
-
-$timer = new Timer();
-
-$timer->add(0.5, Timer::EV_TIMER, function () {
-    echo microtime_float() . "\n";
-});
-
-$timer->add(1, Timer::EV_TIMER_ONCE, function () {
-    echo microtime_float() . "once \n";
-});
-
-//待删除的id
-$id = $timer->add(5, Timer::EV_TIMER, function () {
-    echo "clean up after running once\n";
-});
-
-//删除定时器
-$timer->add(6, Timer::EV_TIMER_ONCE, function () use ($id, $timer) {
-    $timer->del($id, Timer::EV_TIMER);
-});
-
-$timer->loop();
---
+++ test/index.php
@@ -0,0 +1,33 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+namespace timer
+
+//测试执行 timer类
+
+function microtime_float()
+{
+    list($usec, $sec) = explode(" ", microtime());
+    return bcadd($usec, $sec, 3);
+}
+
+$timer = new Timer();
+
+$timer->add(0.5, Timer::EV_TIMER, function () {
+    echo microtime_float() . "\n";
+});
+
+$timer->add(1, Timer::EV_TIMER_ONCE, function () {
+    echo microtime_float() . "once \n";
+});
+
+//待删除的id
+$id = $timer->add(5, Timer::EV_TIMER, function () {
+    echo "clean up after running once\n";
+});
+
+//删除定时器
+$timer->add(6, Timer::EV_TIMER_ONCE, function () use ($id, $timer) {
+    $timer->del($id, Timer::EV_TIMER);
+});
+
+$timer->loop();
